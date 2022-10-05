9cbdc7e4b4b7ba50cd9cbbaf487e4939c4b30099
babytakemyhen <57038606+babytakemyhen@users.noreply.github.com>
2022-10-05 23:49:35 +0700

    Update car.java

---
+++ car.java
@@ -0,0 +1,16 @@
+public class car {
+    int modelYear;
+    String modelName;
+
+    public car(int year, String name){
+        modelYear= year;
+        modelName = name;
+    }
+
+    public static void main(String[] args) {
+        car mycar = new car(1998,"Mustang");
+        System.out.println(mycar.modelYear +" "+ mycar.modelName);
+
+    }
+}
+
