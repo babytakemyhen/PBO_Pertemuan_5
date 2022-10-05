420e87db83d202ebb6134a67559d098268fd15b9
babytakemyhen <57038606+babytakemyhen@users.noreply.github.com>
2022-10-05 23:51:18 +0700

    Update myclass1.java

---
+++ myclass1.java
@@ -0,0 +1,14 @@
+public class myclass1 {
+    int x = 5;
+    public static void main(String[] args) {
+        myclass1 myObj = new myclass1();
+        System.out.println(myObj.x);
+
+        myclass1 myObj1 = new myclass1();
+        System.out.println(myObj1.x);
+        myclass1 myObj2 = new myclass1();
+        System.out.println(myObj2.x);
+    }
+
+}
+
